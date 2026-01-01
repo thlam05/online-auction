8096e31d49a03d703190eb3956e38ddd17aa3685
DAO HOANG DUC MANH <dhdmanh23@clc.fitus.edu.vn>
2026-01-01 11:41:15 +0700

    small ui fix

--- src/server.js
+++ src/server.js
@@ -30,5 +30,5 @@ app.use(errorHandler);
 app.use(errorNotFoundHandler);
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+    console.log(`App listening on http://localhost:${port}`);
 });
